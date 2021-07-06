da55ac45abe1126857b52fdc79e6e34482ad1fca
Sam Peters <sam@sl27.net>
2021-07-06 10:44:24 -0700

    Added /me lambda

---
+++ aws-backend/lambdas/me/me.js
@@ -0,0 +1,21 @@
+const { getSpotifyUserInfo } = require(process.env.AWS ? "/opt/nodejs/utils" : "../../layers/dependencies/utils");
+
+exports.lambdaHandler = async (event, context) => {
+    let response;
+    try {
+        const accessToken = event.requestContext.authorizer.lambda.accessToken
+        const body = await getSpotifyUserInfo(accessToken)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        }
+    } catch (err) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ 
+                "error": err.message,
+                "stackTrace": err.stack
+            })
+        }
+    }
+};
