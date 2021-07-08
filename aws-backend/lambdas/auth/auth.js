baf57ba67db25353fbbc3b81824320132b48dbe6
Sam Peters <sam@sl27.net>
2021-07-07 21:53:38 -0700

    Modified auth function to return jwt in httponly cookie instead of response body

--- aws-backend/lambdas/auth/auth.js
+++ aws-backend/lambdas/auth/auth.js
@@ -6,9 +6,13 @@ const { findCookieValue, verifyJWT } = require(process.env.AWS ? "/opt/nodejs/ut
 exports.lambdaHandler = async (event, context) => {
     let response;
     try {
+        console.log(event);
+        console.log(context);
         const cookies = event.cookies;
         const token = findCookieValue(cookies, "jwt");
         const payload = verifyJWT(token);
+        console.log("payload");
+        console.log(payload);
         response = {
             "isAuthorized": true,
             "context": {
@@ -20,13 +24,10 @@ exports.lambdaHandler = async (event, context) => {
         };       
     } catch (error) {
         console.log(error.message);
+        console.log(JSON.stringify(error.stack));
         response = {
-            "isAuthorized": true,
+            "isAuthorized": false,
             "context": {
-                "scope": payload.scope,
-                "accessToken": payload.accessToken,
-                "refreshToken": payload.refreshToken,
-                "expiresIn": payload.expiresIn
             }
         };
     }
