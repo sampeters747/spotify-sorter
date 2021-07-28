01987c1521294e71b085f54f8606fdc618f302c0
Sam Peters <sam@sl27.net>
2021-07-27 22:23:05 -0700

    Fixed bug causing lambda to exit prematurely

--- aws-backend/lambdas/import-songs/import-songs.js
+++ aws-backend/lambdas/import-songs/import-songs.js
@@ -1,6 +1,5 @@
-const { getUserTracks, createOffsetsArray } = require(process.env.AWS ? "/opt/nodejs/spotify-api-utils" : "../../layers/dependencies/spotify-api-utils");
+const {  createOffsetsArray, createSpotifyApiInstance } = require(process.env.AWS ? "/opt/nodejs/spotify-api-utils" : "../../layers/dependencies/spotify-api-utils");
 const AWS = require('aws-sdk');
-var AWSXRay = require('aws-xray-sdk');
 // const { BatchWrite } = require ('@aws/dynamodb-batch-iterator');
 
 // Setting region
@@ -10,15 +9,54 @@ AWS.config.update({ region: REGION });
 // Tablename
 const TABLENAME = "PlaylistApplicationTable";
 // Create the DynamoDB service object
-var ddb = AWSXRay.captureAWSClient(new AWS.DynamoDB());
+// var ddb = AWSXRay.captureAWSClient(new AWS.DynamoDB());
+var documentClient = new AWS.DynamoDB.DocumentClient();
+
+async function addTrackFeatures(tracks, spotifyApi) {
+    try {
+        const trackIds = tracks.map(track => track.id);
+        const response = await spotifyApi.getAudioFeaturesForTracks(trackIds);
+        const trackFeatures = response.body.audio_features;
+        const updatedUserTracks = tracks.map((track, index) => {
+            let trackWithAudioFeatures = {
+                ...trackFeatures[index],
+                ...track
+            };
+            return trackWithAudioFeatures;
+        });
+        console.log({updatedUserTracks})
+        return updatedUserTracks
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+/**
+ * Helper function that simplifies the nested track object returned by Spotify's API
+ * into a simpler flat object containing only the info we need.
+ * @param {Object} trackObj Original nested trackObject 
+ * @returns {Object} Reduced flat object
+ */
+ function simplifyTrackObject(trackObj) {
+    const track = trackObj.track;
+    let result = {
+        addedAt: trackObj.added_at,
+        id: track.id,
+        title: track.name,
+        artist_name: track.artists[0].name ? track.artists[0].name : "null",
+        preview_url: track.preview_url ? track.preview_url : "null",
+        type: "track"
+    }
+    return result;
+}
+
 
 const createUserSongPutRequest = (song, userId) => {
     const item = {
-        "primaryId": { "S": userId },
-        "secondaryId": { "S": song.id },
-        "title": { "S": song.title },
-        "artist": { "S": song.artist_name },
-        "preview_url": { "S": song.preview_url }
+        "primaryId": userId,
+        "secondaryId": song.id,
+        ...song
     }
     return {
         "PutRequest": {
@@ -27,55 +65,104 @@ const createUserSongPutRequest = (song, userId) => {
     }
 }
 
-async function saveBatch(batch, n) {
-    const res = await ddb.batchWriteItem(batch).promise();
-    return res
-}
-
-async function saveSongs(songs, userId) {
-    const putRequests = songs.map(song => createUserSongPutRequest(song, userId));
-    const offsets = createOffsetsArray(songs.length, 25);
-    const chunks = offsets.map(offset => putRequests.slice(offset, offset + 25));
-    const batchedRequests = chunks.map(chunk => {
-        const batchRequest = {
+async function saveSongBatch(songs, userId) {
+    try {
+        const putRequests = songs.map(song => createUserSongPutRequest(song, userId));
+        const params = {
             RequestItems: {
-                [TABLENAME]: chunk
+                [TABLENAME]: putRequests
             }
         }
-        return batchRequest
-    });
-    console.time("dynamodb");
-    const promises = batchedRequests.map((batch,i) => saveBatch(batch, i));
-    const finished = await Promise.all(promises);
-    console.timeEnd("dynamodb");
-    return finished
-}
-// async function saveSongs(songs, userId) {
-//     const putRequests = songs.map(song => createUserSongPutRequest(song, userId));
-
-//     const keys = putRequests.map(req => [TABLENAME, req]);
-//     let count = 0
-//     for await (const item of new BatchWrite(ddb, keys)) {
-//         count += 1;
-//     }
-//     console.log(`Finished, wrote ${count} user songs to DynamoDB`);
-//     return true
-// }
-
-async function importSongs(accessToken, userId) {
-    try {
-        const songs = await getUserTracks(accessToken);
-        const status = await saveSongs(songs, userId);
-        return songs
+        return await documentClient.batchWrite(params).promise()
     } catch (error) {
         throw error
     }
 }
 
+async function importSongsBatch(userId, offset, spotifyApi) {
+    try {
+        console.log(`starting batch`, offset);
+        // Initial request to get batch of 50 tracks from user's library
+        const response = await spotifyApi.getMySavedTracks({limit: 50, offset: offset});
+        const userTracks = response.body.items;
+        console.log('got tracks for', offset);
+        // Modifying each track object to contain only the data we want
+        const simplifiedUserTracks = userTracks.map(simplifyTrackObject);
+        // Making second batch request to get set of audio feature data for each track
+        const tracksWithAudioFeatures = await addTrackFeatures(simplifiedUserTracks, spotifyApi);
+        console.log(`finished retrieving features for batch ${offset}`);
+        // Since dynamodb has a max batch size of 25, we split our set of songs into 1-2 separate batches
+        // before saving to ddb.
+        let ddbBatches;
+        if (tracksWithAudioFeatures.length > 25 ) {
+            ddbBatches = [tracksWithAudioFeatures.slice(0,25), tracksWithAudioFeatures.slice(25,50)];
+        } else {
+            ddbBatches = [tracksWithAudioFeatures.slice(0,25)];
+        }
+        console.log("starting saving", offset);
+        let saves = ddbBatches.map( batch => saveSongBatch(batch, userId));
+        const finished = await Promise.all(saves)
+        console.log("finished saving", offset);
+        console.log(finished);
+        return finished;
+    } catch (e) {
+        console.log(e);
+        throw e
+    }
+
+}
+
+async function importSongs(accessToken, userId) {
+    try {
+        const spotifyApi = createSpotifyApiInstance(accessToken);
+        let response = await spotifyApi.getMySavedTracks({limit: 1})
+        const total = response.body.total;
+        // const total = 205;
+        console.log(`Importing ${total} songs from user's spotify library`);
+        console.time("import songs");
+        // Creating array of offsets so we don't get the same 50 songs in the users library in every request
+        const offsets = createOffsetsArray(total);
+        console.log(offsets)
+        const batches = offsets.map(offset => {
+            return importSongsBatch(userId, offset, spotifyApi);
+        })
+        console.timeLog("import songs");
+        let result = await Promise.all(batches);
+        console.log(result);
+        console.timeEnd("import songs");
+        return result
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+/**
+ * Determines whether the event came from AWS SNS or AWS Api Gateway and extracts
+ * the user details passed by the event.
+ * @param {Object} event 
+ * @returns Object containing user details
+ */
+function fetchUserDetails(event) {
+    // Checking if a Sns message triggered the lambda
+    if ("Records" in event && "Sns" in event.Records[0]) {
+        // Sns message contains user details
+        return JSON.parse(event.Records[0].Sns.Message)
+    // Checking if a API gateway request triggered the lambda
+    } else if (event.requestContext?.authorizer?.lambda?.accessToken) {
+        // Lambda authorizer context contains user details
+        return event.requestContext?.authorizer?.lambda
+    } else {
+        throw new Error("Was not able to determine what service invoked lambda")
+    }
+}
+
 exports.lambdaHandler = async (event, context) => {
     try {
-        const accessToken = event.requestContext.authorizer.lambda.accessToken;
-        const userId = event.requestContext.authorizer.lambda.id;
+        const user = fetchUserDetails(event);
+        const accessToken = user.accessToken;
+        const userId = user.id;
+        console.log(accessToken, userId, user);
         const songs = await importSongs(accessToken, userId);
         return {
             statusCode: 200,
@@ -87,9 +174,21 @@ exports.lambdaHandler = async (event, context) => {
         return {
             statusCode: 400,
             body: JSON.stringify({
-                "error": err.message,
+                "error": JSON.stringify(err.message),
                 "stackTrace": err.stack
             })
         }
     }
 };
+
+// const code = "BQDfvjZajH_HzUqrBVeFXPBF4BdpwbB5_IGaVjFVhJobyuqczZfU4N7j3rIywvX7UJXcdwYYbHUmT0SI4PydmKF_6wnX0CsdO5rHEbbDBOv7f17gPflsMoIl3dpgtKVrHpfNwVuBlL-IRjbpz0Cmb-w60oSn6A";
+// (async () => {
+//     try {
+//         var text = await importSongs(code, "1");
+//         console.log(text);
+//     } catch (e) {
+//         console.log(e.message);
+//         console.log(e.stack);
+//         // Deal with the fact the chain failed
+//     }
+// })();
--- aws-backend/layers/dependencies/spotify-api-utils.js
+++ aws-backend/layers/dependencies/spotify-api-utils.js
@@ -35,63 +35,14 @@ function filterObject (original, keys) {
 /**
  * Helper function creates an array of offsets to use when requesting portions of
  * a user's library
- * @param {Number} totalTracks Total number of tracks in the user's spotify library 
+ * @param {Number} arraySize Size of the array we're creating offsets for 
  * @returns {Array.<Number>} List of offset numbers
  */
-function createOffsetsArray(totalTracks) {
-    const totalOffsets = Math.ceil(totalTracks/50);
-    const offsets = Array(totalOffsets).fill().map((_, i) => i*50);
+function createOffsetsArray(arraySize, offsetDist=50) {
+    const totalOffsets = Math.ceil(arraySize/offsetDist);
+    const offsets = Array(totalOffsets).fill().map((_, i) => i*offsetDist);
     return offsets
 }
-
-/**
- * Helper function that simplifies a nested track object into a simpler flat object
- * containing only the info we need.
- * @param {Object} trackObj Original nested trackObject 
- * @returns {Object} Reduced flat object
- */
-function simplifyTrackObject(trackObj) {
-    const track = trackObj.track;
-    let result = {
-        addedAt: trackObj.added_at,
-        id: track.id,
-        title: track.name,
-        artist_name: track.artists[0].name,
-        preview_url: track.preview_url,
-    }
-    return result;
-}
-
-/**
- * Retrieves all tracks in a User's spotify library.
- * @param {String} code OAuth2 access token from Spotify's API
- * @returns {Object} Object containing user information
- */
-async function getUserTracks(code) {
-    try {
-        const spotifyApi = createSpotifyApiInstance(code);
-        let response = await spotifyApi.getMySavedTracks({limit: 1})
-        const total = response.body.total;
-        console.log(`Importing ${total} songs from user's spotify library`);
-        // Creating array of offsets so we don't get the same 50 songs in the users library in every request
-        const offsets = createOffsetsArray(total);
-        // Asynchrounously requesting all the tracks in the user's library
-        const apiResponses = offsets.map(offset => {
-            return spotifyApi.getMySavedTracks({limit: 50, offset: offset})
-        });
-        // Waiting for all requests to finish
-        const finishedResponses = await Promise.all(apiResponses);
-        console.log('Finished requesting songs')
-        // Combining the separate track item arrays in each response into a singular array
-        const combinedTrackItems = finishedResponses.flatMap( response => response.body.items);
-        // Simplifying track objects and returning entire array
-        return combinedTrackItems.map(simplifyTrackObject);
-        
-    } catch (error) {
-        throw error
-    }
-}
-
 
 /**
  * Returns a reduced set of user information from the https://api.spotify.com/v1/me
@@ -114,9 +65,8 @@ async function getUserTracks(code) {
 
 
 module.exports = {
-    getUserTracks,
     getSpotifyUserInfo, 
     createSpotifyApiInstance,
-    filterObject, 
+    filterObject,
+    createOffsetsArray,
 }
-
